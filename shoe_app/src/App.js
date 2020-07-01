fc3a9a9346f4563f6c75e8de4d2369b8633fd15e
Janelle Kuhns <j3ckuhns@gmail.com>
2020-07-01 16:24:39 -0700

    final for grading

--- shoe_app/src/App.js
+++ shoe_app/src/App.js
@@ -5,31 +5,33 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      shoe: []
+      shoe: [],
     };
   }
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/shoe/')
+    fetch('http://127.0.0.1:8000/api/Shoe/')
     .then(res => res.json())
     .then(data => this.setState({shoe: data}))
   }
   render() {
     return (
         <div >
+          <h1>My shoestore</h1>
           <ul>
-            {this.state.shoe.map(p => {
+            {this.state.shoe.map((p) => {
               return (
-              <li key={p.id}>
-                Size: {p.size} 
-                Brand: {p.brand_name}
-                Manufacturer: {p.manufacturer} 
-                Color: {p.color} 
-                Material: {p.material} 
-                Shoe Type: {p.shoe_type}
-                Fasten Type: {p.fasten_type}
-              </li>)
+              <p>
+                <li>Size: {p.size}</li>
+                <li>Brand: {p.brand_name}</li>
+                <li>Manufacturer: {p.manufacturer}</li> 
+                <li>Color: {p.color}</li> 
+                <li>Material: {p.material}</li> 
+                <li>Shoe Type: {p.shoe_type}</li>
+                <li>Fasten Type: {p.fasten_type}</li>
+              </p>
+              )
             })}
-            </ul>
+          </ul>
       </div>
     );
   } 
